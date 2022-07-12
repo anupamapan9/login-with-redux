9c2363ff1b9829b5b931f046a45514a7706c6130
ANUPAM MITRA <anupamapan0905@gmail.com>
2022-07-12 21:17:34 +0600

    first commit

---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <section>
+            <form>
+                <h1>Login Here</h1>
+                <input type="name" placeholder='Name' name="" id="" />
+                <input type="email" placeholder='Email' name="" id="" />
+                <input type="password" placeholder='Password' name="" id="" />
+            </form>
+        </section>
+    );
+};
+
+export default Login;
